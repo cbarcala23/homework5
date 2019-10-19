dcfac32b0faa017639729cd1a508cda752c37447
itadmin <itadmin@class.hsd1.wa.comcast.net>
2019-10-19 08:32:10 -0700

    upload 1

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+// a.function to build scheduler Rows(function name buildRows) -> loop, moment.js(maybe)
+function buildRows() {
+
+}
+
+// b.set date at the top of the page(showTodaysDate) -> moment.js
+function showTodaysDate() {
+
+}
+
+// c.save button event handler(function saverow) -> uses localstorage
+function saveRows() {
+
+}
+
+// d.change row styles(function updateRowStyle) -> moment.js (update style for those that are past hours, present hour, future hours)
+
+function updateRowStyle() {
+
+}
+
+// i.on page load, check current time(hour) against rows in scheduler
+function getTime() {
+
+}
+
+//buildRows();
